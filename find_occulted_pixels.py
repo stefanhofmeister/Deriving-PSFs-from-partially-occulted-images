7c45098378ff0ee3f16b57efe10d91ea1ca44f22
stefanhofmeister <47357550+stefanhofmeister@users.noreply.github.com>
2022-11-18 14:23:50 +0100

    Update find_occulted_pixels.py

--- find_occulted_pixels.py
+++ find_occulted_pixels.py
@@ -1,9 +1,6 @@
-"""© 2022 The Trustees of Columbia University in the City of New York.
-This work may be reproduced, distributed, and otherwise exploited for
-academic non-commercial purposes only, provided that it cites the
-original work (IN PUBLICATIONP PROCESS).  To obtain a license to
-use this work for commercial purposes, please contact Columbia
-Technology Ventures at techventures@columbia.edu <mailto:techventures@columbia.edu>. 
+"""© 2022 The Trustees of Columbia University in the City of New York. This work may be reproduced, distributed, and otherwise exploited for academic non-commercial purposes only, provided that it cites the original work: 
+Stefan J. Hofmeister, Michael Hahn, and Daniel Wolf Savin, "Deriving instrumental point spread functions from partially occulted images," J. Opt. Soc. Am. A 39, 2153-2168 (2022), doi: 10.1364/JOSAA.471477.
+To obtain a license to use this work for commercial purposes, please contact Columbia Technology Ventures at techventures@columbia.edu <mailto:techventures@columbia.edu>. 
 """
 
 import numpy as np
