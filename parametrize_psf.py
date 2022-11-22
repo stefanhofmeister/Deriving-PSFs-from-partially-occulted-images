1293e7b748ccca13f7fa5cdce02e814de63f2640
stefanhofmeister <47357550+stefanhofmeister@users.noreply.github.com>
2022-11-22 14:31:21 +0100

    Update parametrize_psf.py

--- parametrize_psf.py
+++ parametrize_psf.py
@@ -36,77 +36,102 @@ def parametrize_psf(config):
     dist_large_psf = np.sqrt(x_large_psf**2 + y_large_psf**2)
     angles_large_psf = np.arctan2(x_large_psf, y_large_psf) / np.pi * 180.
     angles_large_psf += 180
-        
-    #create definitions of the PSF core
-    inner = {}
-    inner['shape'] = config['psf_discretization']['shape']
-    inner['shells_edges'] = config['psf_discretization']['radius_segments_edges'] 
-    inner['radius_min'] = 0
-    inner['radius_max'] = np.sqrt(2) * resolution + 1
-    if 'radius_inner_outer_boundary' in config['psf_discretization']:
-        inner['radius_max'] =  config['psf_discretization']['radius_inner_outer_boundary']
-    index_rad_min = [i for i,v in enumerate(inner['shells_edges']) if v >inner['radius_min']][0] -1
-    index_rad_max = [i for i,v in enumerate(inner['shells_edges']) if v <inner['radius_max']][-1] +1
-    inner['shells_edges'] = inner['shells_edges'][index_rad_min : index_rad_max +1] 
-    inner['n_shells'] = len(inner['shells_edges']) -1
-    if 'shell_segments' in inner['shape']: inner['shell_segments_angle'] = config['psf_discretization']['width_segments_angle']
-    else:                               inner['shell_segments_angle'] = 360
-    inner['n_shellsegments'] = 360 // inner['shell_segments_angle']
-    inner['n_segments'] = inner['n_shells'] * inner['n_shellsegments']
-    
-    #and of the outer PSF region. If the outer one is not define, the core region has to be defined to span the entire PSF.
-    outer = {}
-    if 'radius_inner_outer_boundary' in config['psf_discretization']:
-        outer['shape'] = config['psf_discretization']['outer_shape']
-        outer['shells_edges'] = config['psf_discretization']['outer_radius_segments_edges'] 
-        outer['radius_min'] = config['psf_discretization']['radius_inner_outer_boundary']
-        outer['radius_max'] = np.sqrt(2) * resolution + 1
-        index_rad_min = [i for i,v in enumerate(outer['shells_edges']) if v >outer['radius_min']][0] -1
-        index_rad_max = [i for i,v in enumerate(outer['shells_edges']) if v <outer['radius_max']][-1] +1
-        outer['shells_edges'] = outer['shells_edges'][index_rad_min : index_rad_max +1] 
-        outer['n_shells'] = len(outer['shells_edges']) -1
-        if 'outer_shell_segments' in outer['shape']: outer['shell_segments_angle'] = config['psf_discretization']['outer_width_segments_angle']
-        else:                                        outer['shell_segments_angle'] = 360
-        outer['n_shellsegments'] = 360 // outer['shell_segments_angle']
-        outer['n_segments'] = outer['n_shells'] * outer['n_shellsegments']
+       
+    if ('radius_segments_edges' in  config['psf_discretization']) and ('shape' in  config['psf_discretization']):
+        #create definitions of the PSF core
+        inner = {}
+        inner['shape'] = config['psf_discretization']['shape']
+        inner['shells_edges'] = config['psf_discretization']['radius_segments_edges'] 
+        inner['radius_min'] = 0
+        inner['radius_max'] = np.sqrt(2) * resolution + 1
+        if 'radius_inner_outer_boundary' in config['psf_discretization']:
+            inner['radius_max'] =  config['psf_discretization']['radius_inner_outer_boundary']
+        index_rad_min = [i for i,v in enumerate(inner['shells_edges']) if v >inner['radius_min']][0] -1
+        index_rad_max = [i for i,v in enumerate(inner['shells_edges']) if v <inner['radius_max']][-1] +1
+        inner['shells_edges'] = inner['shells_edges'][index_rad_min : index_rad_max +1] 
+        inner['n_shells'] = len(inner['shells_edges']) -1
+        if 'shell_segments' in inner['shape']: inner['shell_segments_angle'] = config['psf_discretization']['width_segments_angle']
+        else:                               inner['shell_segments_angle'] = 360
+        inner['n_shellsegments'] = 360 // inner['shell_segments_angle']
+        inner['n_segments'] = inner['n_shells'] * inner['n_shellsegments']
+
+        #and of the outer PSF region. If the outer one is not define, the core region has to be defined to span the entire PSF.
+        outer = {}
+        if 'radius_inner_outer_boundary' in config['psf_discretization']:
+            outer['shape'] = config['psf_discretization']['outer_shape']
+            outer['shells_edges'] = config['psf_discretization']['outer_radius_segments_edges'] 
+            outer['radius_min'] = config['psf_discretization']['radius_inner_outer_boundary']
+            outer['radius_max'] = np.sqrt(2) * resolution + 1
+            index_rad_min = [i for i,v in enumerate(outer['shells_edges']) if v >outer['radius_min']][0] -1
+            index_rad_max = [i for i,v in enumerate(outer['shells_edges']) if v <outer['radius_max']][-1] +1
+            outer['shells_edges'] = outer['shells_edges'][index_rad_min : index_rad_max +1] 
+            outer['n_shells'] = len(outer['shells_edges']) -1
+            if 'outer_shell_segments' in outer['shape']: outer['shell_segments_angle'] = config['psf_discretization']['outer_width_segments_angle']
+            else:                                        outer['shell_segments_angle'] = 360
+            outer['n_shellsegments'] = 360 // outer['shell_segments_angle']
+            outer['n_segments'] = outer['n_shells'] * outer['n_shellsegments']
+        else:
+            outer['n_segments'] = 0
+            outer['shape'] = ''
+            outer['n_shells'] = 0
+            outer['n_shellsegments'] = 0
+            outer['shell_segments_angle'] = 0
+            outer['shells_edges'] = 0
+
+        #define an output array
+        n_segments_total = int(inner['n_segments']) + int(outer['n_segments'])
+        shells = {'index': np.full(n_segments_total, -1, dtype = np.int32), 'npix': np.full(n_segments_total, 0, dtype = np.int32), 'radius_mean_px': np.full(n_segments_total, np.nan, dtype = np.float16), 'radius_min_px': np.full(n_segments_total, np.nan, dtype = np.float16), 'radius_max_px': np.full(n_segments_total, np.nan, dtype = np.float16), 'angle_mean': np.full(n_segments_total, np.nan, dtype = np.float16), 'angle_min': np.full(n_segments_total, np.nan, dtype = np.float16), 'angle_max': np.full(n_segments_total, np.nan, dtype = np.float16)}
+
+        #Now, derive the PSF segmentation for the core and outer region
+        index_segment = 0
+        for region in [inner, outer]:
+            if region['n_segments'] == 0: continue
+
+            #Now, create the segments in the psf kernel
+            if region['shape'] == 'shells':
+                #if one uses simple shells, no segmentation of the individual shells is needed
+                for ind_shell in range(region['n_shells']): 
+                    mask_shell = (dist_large_psf >= region['shells_edges'][ind_shell]) & (dist_large_psf < region['shells_edges'][ind_shell+1]) & (dist_large_psf >= region['radius_min']) & (dist_large_psf <= region['radius_max'])
+                    indices_large_psf[mask_shell] = index_segment
+                    shells['index'][index_segment] = index_segment
+                    index_segment += 1
+
+            if region['shape'] == 'shell_segments':    
+                #splits each shell into segments of shell_segments_angle degrees
+                #first create a map of the angles, i.e., of each pixel to the image center
+                #then, create the accroding maps for each angle: first, set the shell segment around angle==0 manually, and use for all others the remaining loop iterations
+                for ind_angle in range(region['n_shellsegments']):
+                    for ind_shell in range(region['n_shells']):
+                        mask_shell = (dist_large_psf >= region['shells_edges'][ind_shell]) & (dist_large_psf < region['shells_edges'][ind_shell+1])  & (dist_large_psf >= region['radius_min']) & (dist_large_psf <= region['radius_max'])
+                        mask_angle = (angles_large_psf >= ind_angle*region['shell_segments_angle']) & (angles_large_psf <= (ind_angle + 1)*region['shell_segments_angle'])
+                        mask_segment = (mask_shell == 1) & (mask_angle == 1)
+                        indices_large_psf[mask_segment] = index_segment
+                        shells['index'][index_segment] = index_segment
+                        index_segment += 1
     else:
-        outer['n_segments'] = 0
+        n_segments_total = 0
+        psf_shape_inner = ''
+        inner = {}
+        inner['shape'] = ''
+        inner['n_shells'] = 0
+        inner['n_shellsegments'] = 0
+        inner['shell_segments_angle'] = np.nan
+        inner['shells_edges'] = []
+        outer = {}
         outer['shape'] = ''
         outer['n_shells'] = 0
         outer['n_shellsegments'] = 0
-        outer['shell_segments_angle'] = 0
-        outer['shells_edges'] = 0
-
-    #define an output array
-    n_segments_total = int(inner['n_segments']) + int(outer['n_segments'])
-    shells = {'index': np.full(n_segments_total, -1, dtype = np.int32), 'npix': np.full(n_segments_total, 0, dtype = np.int32), 'radius_mean_px': np.full(n_segments_total, np.nan, dtype = np.float16), 'radius_min_px': np.full(n_segments_total, np.nan, dtype = np.float16), 'radius_max_px': np.full(n_segments_total, np.nan, dtype = np.float16), 'angle_mean': np.full(n_segments_total, np.nan, dtype = np.float16), 'angle_min': np.full(n_segments_total, np.nan, dtype = np.float16), 'angle_max': np.full(n_segments_total, np.nan, dtype = np.float16)}
-
-    #Now, derive the PSF segmentation for the core and outer region
-    index_segment = 0
-    for region in [inner, outer]:
-        if region['n_segments'] == 0: continue
-            
-        #Now, create the segments in the psf kernel
-        if region['shape'] == 'shells':
-            #if one uses simple shells, no segmentation of the individual shells is needed
-            for ind_shell in range(region['n_shells']): 
-                mask_shell = (dist_large_psf >= region['shells_edges'][ind_shell]) & (dist_large_psf < region['shells_edges'][ind_shell+1]) & (dist_large_psf >= region['radius_min']) & (dist_large_psf <= region['radius_max'])
-                indices_large_psf[mask_shell] = index_segment
-                shells['index'][index_segment] = index_segment
-                index_segment += 1
-    
-        if region['shape'] == 'shell_segments':    
-            #splits each shell into segments of shell_segments_angle degrees
-            #first create a map of the angles, i.e., of each pixel to the image center
-            #then, create the accroding maps for each angle: first, set the shell segment around angle==0 manually, and use for all others the remaining loop iterations
-            for ind_angle in range(region['n_shellsegments']):
-                for ind_shell in range(region['n_shells']):
-                    mask_shell = (dist_large_psf >= region['shells_edges'][ind_shell]) & (dist_large_psf < region['shells_edges'][ind_shell+1])  & (dist_large_psf >= region['radius_min']) & (dist_large_psf <= region['radius_max'])
-                    mask_angle = (angles_large_psf >= ind_angle*region['shell_segments_angle']) & (angles_large_psf <= (ind_angle + 1)*region['shell_segments_angle'])
-                    mask_segment = (mask_shell == 1) & (mask_angle == 1)
-                    indices_large_psf[mask_segment] = index_segment
-                    shells['index'][index_segment] = index_segment
-                    index_segment += 1
+        outer['shell_segments_angle'] = np.nan
+        outer['shells_edges'] = []      
+        shells = {}
+        shells['index'] = []
+        shells['npix'] = []        
+        shells['radius_mean_px'] = []
+        shells['radius_min_px'] = []
+        shells['radius_max_px'] = []
+        shells['angle_mean'] = [] 
+        shells['angle_min'] = []
+        shells['angle_max'] = []
 
     
     #if a manual discretization file was provided, use the segments defined there to overwrite the shells and shell segments defined before. This allows to stack a manual segmentation, e.g., for the diffraction pattern, with simple shells.
