5b427a614e41333e074c47d5335fad244dae472c
stefanhofmeister <47357550+stefanhofmeister@users.noreply.github.com>
2022-12-15 16:36:34 +0100

    Update find_occulted_pixels.py

--- find_occulted_pixels.py
+++ find_occulted_pixels.py
@@ -13,7 +13,9 @@ import numpy as np
 import glob
 import os
 from inout import read_image, save_image
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.colors import Normalize, LogNorm 
 
 #This is the top level routine for identifying the occulted pixels
 def find_occulted_pixels(config):
@@ -23,40 +25,68 @@ def find_occulted_pixels(config):
     else: add_distance = 0
 
     #if occultation masks have been predefined
-    if method == 'predefined':
-        files = glob.glob(folder_run + '/occultation_mask/*.npz')
-        for file in files:
+    if method == 'predefined': files = glob.glob(folder_run + '/occultation_mask/*.npz')
+    else:                      files = glob.glob(folder_run + '/deconvolved_image/*.npz')
+        
+    for file in files:        
+        #here, you can also define your own methods for finding the occulted pixels
+        if method == 'predefined':        
+            #if the occultation mask has been provided by the user
             occultation_mask = read_image(file, dtype = np.int8)
-            plot_image(occultation_mask, file, plot_norm = 'lin') 
+            img = read_image(folder_run + '/deconvolved_image/' + os.path.splitext(os.path.basename(file))[0] + '.npz')
             
-    #if the occultation masks have not been predefined and provided by the user: derive the occultation masks
-    if method != 'predefined':  #if the occultation masks have not been predefined and provided by the user: derive the occultation masks
-        #derive the occultation masks
-        files = glob.glob(folder_run + '/deconvolved_image/*.npz')
-        for file in files:
+        if method == 'threshold':            
+            #derive the occultation masks by an adaptive threshold technique
             img = read_image(file, dtype = np.float16)  
-            #here, you can also define your own methods for finding the occulted pixels
-            if method == 'threshold':        
-                thr        = config['occultation_mask']['threshold']
-                use_median = config['occultation_mask']['threshold_usemedian'] 
-                occultation_mask = find_occulted_pixels_bythr(img, thr, use_median)
+            thr        = config['occultation_mask']['threshold']
+            use_median = config['occultation_mask']['threshold_usemedian'] 
+            occultation_mask = find_occulted_pixels_bythr(img, thr, use_median)
             
-            if add_distance > 0:  
-                kernel_size = 2 * np.floor(add_distance).astype(np.int) + 1
-                kx, ky = np.meshgrid(np.arange(kernel_size) - kernel_size//2, np.arange(kernel_size) - kernel_size//2)
-                kr = kx**2 + ky**2
-                kernel = np.zeros((kernel_size, kernel_size))
-                kernel[kr <= add_distance**2] = 1
-                #do a binary erosion but with edge mode = 'nearest'
-                mask_ignore = (occultation_mask == -1)
-                occultation_mask = np.logical_not(occultation_mask)
-                occultation_mask = ndimage.convolve(occultation_mask.astype(np.int8), kernel.astype(np.int8), mode = 'nearest')
-                occultation_mask = np.logical_not(occultation_mask).astype(np.int8)
-                occultation_mask[mask_ignore] = -1
-                
-            save_image(occultation_mask, folder_run + '/occultation_mask/' + os.path.basename(file), plot_norm = 'lin', dtype = np.int8, keys = 'img')
+        if method == 'eclipse':                  
+            #derive the occultation masks by fitting a circle to the eclipse
+            img = read_image(file, dtype = np.float16)  
+            # thr        = config['occultation_mask']['threshold']
+            # use_median = config['occultation_mask']['threshold_usemedian'] 
+            # occultation_mask = find_occulted_pixels_byeclipse(img, thr, use_median)
+            occultation_mask = fit_eclipse( config, file, thr_eclipse =0.10, morph_radius = 5, distance_from_eclipse_boundary = 20, low_resolution_for_fit = 4096, high_resolution_for_fit = 4096, erosion = 0) 
+            # occultation_mask[occultation_mask > 0] = 1
+            # occultation_mask[(occultation_mask <= 0) & (occultation_mask != -1)] = 0
+	                      
+        if add_distance > 0:  
+            kernel_size = 2 * np.floor(add_distance).astype(np.int) + 1
+            kx, ky = np.meshgrid(np.arange(kernel_size) - kernel_size//2, np.arange(kernel_size) - kernel_size//2)
+            kr = kx**2 + ky**2
+            kernel = np.zeros((kernel_size, kernel_size))
+            kernel[kr <= add_distance**2] = 1
+            mask_ignore = (occultation_mask == -1)
+            occultation_mask = np.logical_not(occultation_mask)
+            occultation_mask = ndimage.convolve(occultation_mask.astype(np.int8), kernel.astype(np.int8), mode = 'nearest')
+            occultation_mask = np.logical_not(occultation_mask).astype(np.int8)
+            occultation_mask[mask_ignore] = -1
 
+        #plot the occultation mask, one time solely the occultation mask and one time overlayed to the image
+        filename_figure = os.path.splitext(file)[0] + '.jpg'
+        fig, ax = plt.subplots(1, 2, figsize = (12, 5)) 
+        ax1 = ax[0] 
+        divider = make_axes_locatable(ax1) 
+        cax1 = divider.append_axes('right', size='5%', pad=0.05)
+        im1 = ax1.imshow(occultation_mask, norm = Normalize(vmin = -1, vmax = 1, clip = True), interpolation = 'nearest', cmap = 'viridis')
+        fig.colorbar(im1, cax=cax1, orientation='vertical', ticks=[-1, 0, 1])
+        ax2 = ax[1] 
+        divider = make_axes_locatable(ax2) 
+        cax2 = divider.append_axes('right', size='5%', pad=0.05)
+        img_occ_marked = np.copy(img)
+        img_occ_marked[occultation_mask == 1] = 10000
+        im2 = ax2.imshow(img_occ_marked,  norm=LogNorm(vmin=1, vmax=10000, clip = True), interpolation = 'nearest', cmap = 'viridis')
+        fig.colorbar(im2, cax=cax2, orientation='vertical')
+        plt.tight_layout()
+        plt.savefig(filename_figure, dpi = 600)
+        plt.close(fig)
+        #finally, save the occultation mask
+        save_image(occultation_mask, folder_run + '/occultation_mask/' + os.path.basename(file), plot = False, dtype = np.int8, keys = 'img')
 
+        
+        
 #In this section, you can put your own algorithms to identify occulted pixels. The function should use the (psf deconvolved) image "img" as input, and return a numpy array with a zero background and the pixels identified as occulted set to one.
 
 def find_occulted_pixels_bythr(img, thr, use_median = True):
